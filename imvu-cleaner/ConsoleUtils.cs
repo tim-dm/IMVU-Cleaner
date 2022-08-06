eb99b53f55720d3ab9dc175be2ce5ff151382d7f
tim-dm <imvu_data@zoho.com>
2022-08-06 01:43:07 -0700

    Create ConsoleUtils.cs

---
+++ imvu-cleaner/ConsoleUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMVU_Cleaner
+{
+    public static class ConsoleUtils
+    {
+        public static void WriteLineColored(ConsoleColor color, string text)
+        {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = originalColor;
+        }
+    }
+}
