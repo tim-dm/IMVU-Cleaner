69c7dcc13f34104ecc588e491f212d11174f0699
tim-dm <imvu_data@zoho.com>
2022-08-06 01:42:59 -0700

    Better implemented SRP
    Better implemented SRP by moving the input handling and cleaner creation into there own classes. Removed unused properties and fields.

--- imvu-cleaner/Program.cs
+++ imvu-cleaner/Program.cs
@@ -4,155 +4,56 @@ using System.IO;
 
 namespace IMVU_Cleaner
 {
-    public enum MenuOptions
-    {
-        ClassicClientCache =  1,
-        ClassicClientLogs =  2,
-        ProjectFiles =  3,
-        All =  4,
-        Exit =  5,
-    }
-
     class Program
     {
-        /// <summary>
-        /// The root location of the IMVU directories
-        /// </summary>
-        private static readonly string _appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-
-        /// <summary>
-        /// The path to the IMVU directory
-        /// </summary>
-        private static readonly string _imvuPath = Path.Join(_appData, @"\IMVU\");
-
-        /// <summary>
-        /// The path to the IMVUClient directory
-        /// </summary>
-        private static readonly string _clientPath = Path.Join(_appData, @"\IMVUClient\");
-
-        /// <summary>
-        /// Flag to determine if we're waiting for input
-        /// </summary>
-        private static bool _idle = true;
-        
-        /// <summary>
-        /// Flag to determine if we should exit the program
-        /// </summary>
-        private static bool _exit = false;
-
-        /// <summary>
-        /// The collection of <see cref="ICleaner" /> to run
-        /// </summary>
-        private static readonly List<ICleaner> _cleaners = new();
-
         static void Main(string[] args)
         {
             Console.Title = "IMVU Cleaner v1.0 by Datamine";
 
-            DisplayMenu();
+            Menu menu = new();
+            menu.Display();
 
-            while (_idle)
+            MenuOption option;            
+
+            do
             {
-                ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                option = menu.GetSelectedOption();
+            }
+            while (option == MenuOption.Empty);
 
-                if (!char.IsNumber(pressedKey.KeyChar))
+            Console.WriteLine(option);    
+
+            if (option == MenuOption.Exit)
+            {
+                Environment.Exit(0);
+            }
+
+            foreach (ICleaner cleaner in CleanerManager.GetCleaners(option))
+            {
+                try
                 {
-                    continue;
+                    cleaner.Clean();
                 }
-
-                if (int.TryParse(pressedKey.KeyChar.ToString(), out int number))
+                catch (Exception ex)
                 {
-                    switch (number)
-                    {
-                        case (int)MenuOptions.ClassicClientCache:
-                            _cleaners.Add(new ClassicCacheCleaner(_imvuPath, _clientPath));
-                            _idle = false;
-                            break;
-
-                        case (int)MenuOptions.ClassicClientLogs:                            
-                            _cleaners.Add(new ClassicLogCleaner(_imvuPath));
-                            _idle = false;
-                            break;
-
-                        case (int)MenuOptions.ProjectFiles:
-                            _cleaners.Add(new ProjectCleaner());
-                            _idle = false;
-                            break;
-
-                        case (int)MenuOptions.All:                            
-                            _cleaners.Add(new ClassicCacheCleaner(_imvuPath, _clientPath));
-                            _cleaners.Add(new ClassicLogCleaner(_imvuPath));
-                            _cleaners.Add(new ProjectCleaner());
-                            break;
-
-                        case (int)MenuOptions.Exit:
-                            _idle = false;
-                            _exit = true;
-                            break;
-                    }
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
                 }
             }
 
-            if(!_exit)
-            {
-                Console.Clear();
-
-                foreach (ICleaner cleaner in _cleaners)
-                {
-                    try
-                    {
-                        cleaner.Clean();
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                        Console.ReadLine();
-                    }
-                }
-
-                //Console.ReadLine();
-                DisplayExitScreen();
-            }
-        }
-
-        private static void DisplayMenu()
-        {
-            Console.Clear();
-            WriteLineColored(ConsoleColor.Cyan, "╔====================================================╗");
-            WriteLineColored(ConsoleColor.Cyan, "║        Advanced IMVU Cleaner by Dataminer          ║");
-            WriteLineColored(ConsoleColor.Cyan, "║        ----------------------------------          ║");
-            WriteLineColored(ConsoleColor.Cyan, "║          Please make sure IMVU is closed           ║");
-            WriteLineColored(ConsoleColor.Cyan, "║                                                    ║");
-            WriteLineColored(ConsoleColor.Cyan, "║  Please selection an option from the menu below    ║");
-            WriteLineColored(ConsoleColor.Cyan, "║       by pressing a number on your keyboard        ║");
-            WriteLineColored(ConsoleColor.Cyan, "║                                                    ║");
-            WriteLineColored(ConsoleColor.Cyan, "║             1. Delete Client Cache                 ║");
-            WriteLineColored(ConsoleColor.Cyan, "║             2. Delete Client Logs                  ║");
-            WriteLineColored(ConsoleColor.Cyan, "║             3. Project Files                       ║");
-            WriteLineColored(ConsoleColor.Cyan, "║             4. Delete All                          ║");
-            WriteLineColored(ConsoleColor.Cyan, "║             5. Exit                                ║");
-            WriteLineColored(ConsoleColor.Cyan, "║                                                    ║");
-            WriteLineColored(ConsoleColor.Cyan, "╚====================================================╝");
+            DisplayExitScreen();
         }
 
         private static void DisplayExitScreen()
         {
-            WriteLineColored(ConsoleColor.Green, "╔==============================================╗");
-            WriteLineColored(ConsoleColor.Green, "║                                              ║");
-            WriteLineColored(ConsoleColor.Green, "║                  COMPLETED                   ║");
-            WriteLineColored(ConsoleColor.Green, "║                                              ║");
-            WriteLineColored(ConsoleColor.Green, "║             PRESS ANY KEY TO EXIT            ║");
-            WriteLineColored(ConsoleColor.Green, "║                                              ║");
-            WriteLineColored(ConsoleColor.Green, "╚==============================================╝");
+            ConsoleUtils.WriteLineColored(ConsoleColor.Green, "╔==============================================╗");
+            ConsoleUtils.WriteLineColored(ConsoleColor.Green, "║                                              ║");
+            ConsoleUtils.WriteLineColored(ConsoleColor.Green, "║                  COMPLETED                   ║");
+            ConsoleUtils.WriteLineColored(ConsoleColor.Green, "║                                              ║");
+            ConsoleUtils.WriteLineColored(ConsoleColor.Green, "║             PRESS ANY KEY TO EXIT            ║");
+            ConsoleUtils.WriteLineColored(ConsoleColor.Green, "║                                              ║");
+            ConsoleUtils.WriteLineColored(ConsoleColor.Green, "╚==============================================╝");
             Console.ReadLine();
-        }
-
-        private static void WriteLineColored(ConsoleColor color, string text)
-        {
-            ConsoleColor originalColor = Console.ForegroundColor;
-            Console.ForegroundColor = color;
-            Console.WriteLine(text);
-            Console.ForegroundColor = originalColor;
-        }
+        }        
     }
 }
